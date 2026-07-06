2506ccd5758f7223e25cdc85f5f1248e1ea9d1ac
Prabhu-Acharya <prabhu.acharya16@gmail.com>
2026-07-06 13:09:56 +0530

    Added Spring Core Maven exercise

---
+++ week-2/Spring_Core_Maven/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,12 @@
+package com.library.repository;
+
+public class BookRepository {
+
+    public void displayRepository() {
+
+        System.out.println(
+                "BookRepository: Repository Layer Working"
+        );
+
+    }
+}
